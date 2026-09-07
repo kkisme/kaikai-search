93c6687ec254fd8fed5335ea2a5792cae49fd658
刘凯 <367270327@qq.com>
2026-09-07 17:18:38 +0800

    Use black result navigation with white text

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'kaikai-search-v7-20260907-control-opacity';
-const ASSETS = ['./','./index.html','./app.css?v=20260907-control-opacity','./app.js?v=20260907-control-opacity','./search.js','./question-bank.json','./manifest.json','./icons/icon.svg','./icons/developer-logo.webp'];
+const CACHE = 'kaikai-search-v8-20260907-black-nav';
+const ASSETS = ['./','./index.html','./app.css?v=20260907-black-nav','./app.js?v=20260907-black-nav','./search.js','./question-bank.json','./manifest.json','./icons/icon.svg','./icons/developer-logo.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('kaikai-search-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
